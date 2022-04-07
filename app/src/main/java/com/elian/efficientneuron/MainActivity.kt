925fc4d89e560d9cf5602f76e1ca7fa49f9b08f5
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-07 19:50:36 +0200

    Update MainActivity.kt

--- app/src/main/java/com/elian/efficientneuron/MainActivity.kt
+++ app/src/main/java/com/elian/efficientneuron/MainActivity.kt
@@ -2,9 +2,11 @@ package com.elian.efficientneuron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.elian.efficientneuron.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelectedListener
 {
     private lateinit var binding: ActivityMainBinding
 
@@ -15,4 +17,26 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    override fun onStart()
+    {
+        super.onStart()
+        
+        initUI()
+    }
+    
+    private fun initUI()
+    {
+        //binding.navigationView.setNavigationItemSelectedListener(this)
+    }
+    
+    // TODO: implement navigation with navigation view
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean
+//    {
+//        when(item.itemId)
+//        {
+//            
+//        }
+//    }
 }
