97c36f475f50c84d1e4578ec0f854fbce65bf926
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-08 19:44:40 +0200

    .

--- app/src/main/java/com/elian/efficientneuron/MainActivity.kt
+++ app/src/main/java/com/elian/efficientneuron/MainActivity.kt
@@ -2,13 +2,20 @@ package com.elian.efficientneuron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.elian.efficientneuron.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelectedListener
 {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toogle: ActionBarDrawerToggle
+
+    //region Activity Methods
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -21,15 +28,58 @@ class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelec
     override fun onStart()
     {
         super.onStart()
-        
+
         initUI()
     }
-    
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        if (toogle.onOptionsItemSelected(item)) return true
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    //endregion
+
+    //region Methods
+
     private fun initUI()
     {
-        //binding.navigationView.setNavigationItemSelectedListener(this)
+        toogle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        binding.navigationView.setNavigationItemSelectedListener(::setNavigation)
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
     }
-    
+
+    private fun setNavigation(item: MenuItem): Boolean
+    {
+        when (item.itemId)
+        {
+            //R.id.navProfile -> goToFragment()
+        }
+        return true
+    }
+
+    private fun goToFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment, fragment)
+            commit()
+        }
+    }
+
+    //endregion
+
+
     // TODO: implement navigation with navigation view
 
 //    override fun onNavigationItemSelected(item: MenuItem): Boolean
