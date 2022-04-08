a08788f0987db3c00298ae9953eebc9b5d465497
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-08 22:53:32 +0200

    Update MainActivity.kt

--- app/src/main/java/com/elian/efficientneuron/MainActivity.kt
+++ app/src/main/java/com/elian/efficientneuron/MainActivity.kt
@@ -2,7 +2,6 @@ package com.elian.efficientneuron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -10,12 +9,11 @@ import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.elian.efficientneuron.databinding.ActivityMainBinding
 import com.elian.efficientneuron.ui.profile.ProfileFragment
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelectedListener
 {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var toogle: ActionBarDrawerToggle
+    private lateinit var toggle: ActionBarDrawerToggle
 
     //region Activity Methods
 
@@ -36,7 +34,7 @@ class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelec
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
-        if (toogle.onOptionsItemSelected(item)) return true
+        if (toggle.onOptionsItemSelected(item)) return true
 
         return super.onOptionsItemSelected(item)
     }
@@ -47,15 +45,15 @@ class MainActivity : AppCompatActivity()//, NavigationView.OnNavigationItemSelec
 
     private fun initUI()
     {
-        toogle = ActionBarDrawerToggle(
+        toggle = ActionBarDrawerToggle(
             this,
             binding.drawerLayout,
             R.string.open_drawer,
             R.string.close_drawer
         )
 
-        binding.drawerLayout.addDrawerListener(toogle)
-        toogle.syncState()
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         binding.navigationView.setNavigationItemSelectedListener(::setNavigation)
 
