aa1ee31bb57cf7f784def4de91041e05bdc0a9ec
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-22 18:29:16 +0200

    Update ProfileFragment.kt
    ViewBinding added

--- app/src/main/java/com/elian/efficientneuron/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/elian/efficientneuron/ui/profile/ProfileFragment.kt
@@ -6,16 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elian.efficientneuron.R
+import com.elian.efficientneuron.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment()
 {
+    private lateinit var binding: FragmentProfileBinding
+    
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
     }
 }
