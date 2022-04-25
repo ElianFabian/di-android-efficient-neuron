72442c62228a5fba125abdcc9afa13e3fcd01fb1
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-25 16:41:52 +0200

    Update FragmentExtension.kt

--- app/src/main/java/com/elian/efficientneuron/extension/FragmentExtension.kt
+++ app/src/main/java/com/elian/efficientneuron/extension/FragmentExtension.kt
@@ -1,16 +1,17 @@
 package com.elian.efficientneuron.extension
 
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 
 
-fun Fragment.navigate(action: Int)
+fun Fragment.navigate(@IdRes action: Int)
 {
     NavHostFragment.findNavController(this).navigate(action)
 }
 
-fun Fragment.navigate(action: Int, args: Bundle?)
+fun Fragment.navigate(@IdRes action: Int, args: Bundle?)
 {
     NavHostFragment.findNavController(this).navigate(action, args)
 }
