1e28f8fd03536e2f22b528af2206b5b30fba1f8b
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-10-16 01:48:55 +0200

    Update LoginUseCase.kt

--- app/src/main/java/com/elian/computeit/feature_auth/domain/use_case/LoginUseCase.kt
+++ app/src/main/java/com/elian/computeit/feature_auth/domain/use_case/LoginUseCase.kt
@@ -1,9 +1,8 @@
 package com.elian.computeit.feature_auth.domain.use_case
 
 import com.elian.computeit.core.domain.util.HashingUtil
-import com.elian.computeit.feature_auth.domain.repository.AuthRepository
 import com.elian.computeit.feature_auth.domain.models.LoginResult
-import com.elian.computeit.core.domain.util.ValidationUtil
+import com.elian.computeit.feature_auth.domain.repository.AuthRepository
 import com.elian.computeit.feature_auth.presentation.util.AuthError
 import javax.inject.Inject
 
@@ -13,8 +12,8 @@ class LoginUseCase @Inject constructor(
 {
     suspend operator fun invoke(email: String, password: String): LoginResult
     {
-        val emailError = if(email.isBlank()) AuthError.ValueEmpty else null
-        val passwordError = if(password.isBlank()) AuthError.ValueEmpty else null
+        val emailError = if (email.isBlank()) AuthError.ValueEmpty else null
+        val passwordError = if (password.isBlank()) AuthError.ValueEmpty else null
 
         if (emailError != null || passwordError != null)
         {
