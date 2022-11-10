520378209a630906183ad247aa969d759b8bacc0
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-10 17:44:59 +0100

    Added transitions to navigation

--- app/src/main/java/com/elian/computeit/core/util/extensions/FragmentExt.kt
+++ app/src/main/java/com/elian/computeit/core/util/extensions/FragmentExt.kt
@@ -8,12 +8,9 @@ import androidx.annotation.IdRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
-fun Fragment.navigate(@IdRes action: Int, args: Bundle? = null)
-{
-    NavHostFragment.findNavController(this).navigate(action, args)
-}
+fun Fragment.navigate(@IdRes action: Int, args: Bundle? = null) = findNavController().navigate(action, args)
 
 fun Fragment.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT)
 {
