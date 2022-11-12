9f2a99d283ead197a661cd624387a9032a98187f
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-12 23:36:07 +0100

    Update Resource.kt

--- app/src/main/java/com/elian/computeit/core/util/Resource.kt
+++ app/src/main/java/com/elian/computeit/core/util/Resource.kt
@@ -1,6 +1,5 @@
 package com.elian.computeit.core.util
 
-// From: https://github.com/philipplackner/SocialNetworkTwitch/blob/development/app/src/main/java/com/plcoding/socialnetworktwitch/core/util/Resource.kt
 typealias SimpleResource = Resource<Unit>
 
 sealed class Resource<T>(val data: T? = null, val uiText: UiText? = null)
