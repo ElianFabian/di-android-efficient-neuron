5f51070d8af2b9ddf3b513bad229b5f4e80f16e6
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-12 22:14:54 +0100

    Update ActivityExt.kt

--- app/src/main/java/com/elian/computeit/core/util/extensions/ActivityExt.kt
+++ app/src/main/java/com/elian/computeit/core/util/extensions/ActivityExt.kt
@@ -3,7 +3,6 @@ package com.elian.computeit.core.util.extensions
 import android.app.Activity
 import android.content.Intent
 import android.widget.Toast
-import androidx.annotation.StringRes
 
 inline fun <reified T : Activity> Activity.navigateTo()
 {
@@ -14,9 +13,4 @@ inline fun <reified T : Activity> Activity.navigateTo()
 fun Activity.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT)
 {
     Toast.makeText(this, text, duration).show()
-}
-
-fun Activity.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT)
-{
-    Toast.makeText(this, resId, duration).show()
 }
