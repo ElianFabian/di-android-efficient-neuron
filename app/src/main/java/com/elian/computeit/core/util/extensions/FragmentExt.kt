9e1ca8d28ab476d5b6ae9d625365a665c5151a9e
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-12 22:15:09 +0100

    Update FragmentExt.kt

--- app/src/main/java/com/elian/computeit/core/util/extensions/FragmentExt.kt
+++ app/src/main/java/com/elian/computeit/core/util/extensions/FragmentExt.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.ColorRes
 import androidx.annotation.IdRes
-import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -29,11 +28,6 @@ fun Fragment.navigateSafe(
 fun Fragment.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT)
 {
     Toast.makeText(context, text, duration).show()
-}
-
-fun Fragment.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT)
-{
-    Toast.makeText(context, resId, duration).show()
 }
 
 fun Fragment.getColor2(@ColorRes id: Int) = ContextCompat.getColor(requireContext(), id)
