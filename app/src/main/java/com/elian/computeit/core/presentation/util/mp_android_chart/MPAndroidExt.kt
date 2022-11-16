5de3be44c92f68dd3ce5f0aeb4378006a897e34c
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-16 15:12:06 +0100

    Update MPAndroidExt.kt

--- app/src/main/java/com/elian/computeit/core/presentation/util/mp_android_chart/MPAndroidExt.kt
+++ app/src/main/java/com/elian/computeit/core/presentation/util/mp_android_chart/MPAndroidExt.kt
@@ -4,4 +4,6 @@ import com.github.mikephil.charting.data.Entry
 
 fun Map<out Number, Number>.toEntries() = this.map { Entry(it.key.toFloat(), it.value.toFloat()) }
 
-fun List<Number>.toEntries() = this.mapIndexed { index, number -> Entry(index.toFloat(), number.toFloat()) }
+fun List<Number>.toEntries(firstValue: Int = 1) = this.mapIndexed { index, number ->
+    Entry(index.toFloat() + firstValue, number.toFloat())
+}
