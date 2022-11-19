a5fb2d42df060777eb355ad94c7b8335107ef9bf
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-19 20:25:11 +0100

    Moved InitialActivity

--- app/src/main/java/com/elian/computeit/core/InitialActivity.kt
+++ app/src/main/java/com/elian/computeit/core/InitialActivity.kt
@@ -1,10 +1,11 @@
-package com.elian.computeit.core.presentation
+package com.elian.computeit.core
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.elian.computeit.core.domain.repository.LocalAppDataRepository
 import com.elian.computeit.core.domain.util.extensions.isUserLoggedIn
+import com.elian.computeit.core.presentation.MainActivity
 import com.elian.computeit.core.presentation.util.extensions.navigateTo
 import com.elian.computeit.feature_auth.presentation.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
