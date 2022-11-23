14a0a1ee606c60fb86a2126e0c3c600571f71ffd
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-24 00:41:57 +0100

    Refactored User profilePicUrl field

--- app/src/main/java/com/elian/computeit/feature_profile/domain/model/ProfileInfo.kt
+++ app/src/main/java/com/elian/computeit/feature_profile/domain/model/ProfileInfo.kt
@@ -2,6 +2,6 @@ package com.elian.computeit.feature_profile.domain.model
 
 data class ProfileInfo(
     val username: String,
-    val profilePicUrl: String,
+    val profilePicUrl: String?,
     val createdAt: String,
 )
