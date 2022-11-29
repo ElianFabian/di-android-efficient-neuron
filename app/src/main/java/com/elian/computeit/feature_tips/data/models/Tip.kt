2493e38911a2f8f14fa6a0516acfba3aa6e20f46
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-11-29 20:29:19 +0100

    Removed unsued dependencies

--- app/src/main/java/com/elian/computeit/feature_tips/data/models/Tip.kt
+++ app/src/main/java/com/elian/computeit/feature_tips/data/models/Tip.kt
@@ -1,15 +1,10 @@
 package com.elian.computeit.feature_tips.data.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "tip_table")
 data class Tip(
 	val title: String,
 	val example: String,
-
-	@PrimaryKey(autoGenerate = true)
 	val id: Int = 0,
 ) :
 	Serializable
