6295eb9b2b49c0e5df449bcfbdd82d5277b2e56c
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-12-01 15:33:12 +0100

    Update ArgKeys.kt

--- app/src/main/java/com/elian/computeit/core/util/constants/ArgKeys.kt
+++ app/src/main/java/com/elian/computeit/core/util/constants/ArgKeys.kt
@@ -1,6 +1,6 @@
 package com.elian.computeit.core.util.constants
 
-//region Test Configuration -> Test End
+//region Test Configuration -> Test
 
 /** @see com.elian.computeit.feature_tests.data.models.Operation */
 const val EXTRA_OPERATION_TYPE = "EXTRA_OPERATION_TYPE"
@@ -10,6 +10,10 @@ const val EXTRA_OPERATION_NUMBER_RANGE = "EXTRA_OPERATION_NUMBER_RANGE"
 
 /** @see Int */
 const val EXTRA_TEST_TIME_IN_SECONDS = "EXTRA_TEST_TIME_IN_SECONDS"
+
+//endregion
+
+//region Test -> Test End
 
 /** @see com.elian.computeit.feature_tests.domain.model.TestInfo */
 const val EXTRA_TEST_INFO = "EXTRA_TEST_INFO"
