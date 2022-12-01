2a53049f7e70faf09b04c921a8f569f5bda689d5
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-12-01 02:29:25 +0100

    Fixed GenericAdapter

--- app/src/main/java/com/elian/computeit/core/presentation/adapter/GenericAdapter.kt
+++ app/src/main/java/com/elian/computeit/core/presentation/adapter/GenericAdapter.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 class GenericAdapter<ItemT : Any, VB : ViewBinding>(
-	private val inflate: (LayoutInflater) -> VB,
+	private val inflate: (LayoutInflater, ViewGroup, Boolean) -> VB,
 	private val onBind: VB.(ItemT) -> Unit,
 ) : ListAdapter<ItemT, GenericAdapter<ItemT, VB>.ViewHolder>(
 	object : DiffUtil.ItemCallback<ItemT>()
@@ -29,7 +29,7 @@ class GenericAdapter<ItemT : Any, VB : ViewBinding>(
 	{
 		val inflater = LayoutInflater.from(parent.context)
 
-		val binding = inflate(inflater)
+		val binding = inflate(inflater, parent, false)
 
 		return ViewHolder(binding.root, binding)
 	}
@@ -41,3 +41,37 @@ class GenericAdapter<ItemT : Any, VB : ViewBinding>(
 		onBind(holder.binding, item)
 	}
 }
+
+//class GenericAdapter<ItemT : Any, VB : ViewBinding>(
+//	@LayoutRes private val layoutRes: Int,
+//	private val bind: (View) -> VB,
+//	private val onBind: VB.(ItemT) -> Unit,
+//) : ListAdapter<ItemT, GenericAdapter<ItemT, VB>.ViewHolder>(
+//	object : DiffUtil.ItemCallback<ItemT>()
+//	{
+//		override fun areItemsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
+//
+//		@SuppressLint("DiffUtilEquals")
+//		override fun areContentsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
+//	}
+//)
+//{
+//	inner class ViewHolder(view: View, val binding: VB) : RecyclerView.ViewHolder(view)
+//
+//
+//	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+//	{
+//		val view = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
+//
+//		val binding = bind(view)
+//
+//		return ViewHolder(binding.root, binding)
+//	}
+//
+//	override fun onBindViewHolder(holder: ViewHolder, position: Int)
+//	{
+//		val item = getItem(position)
+//
+//		onBind(holder.binding, item)
+//	}
+//}
