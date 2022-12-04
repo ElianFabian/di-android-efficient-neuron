7525181002e1f25222bdf1a2e57829ec833b7cb0
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-12-04 01:30:15 +0100

    Update GenericAdapter.kt

--- app/src/main/java/com/elian/computeit/core/presentation/adapter/GenericAdapter.kt
+++ app/src/main/java/com/elian/computeit/core/presentation/adapter/GenericAdapter.kt
@@ -1,6 +1,5 @@
 package com.elian.computeit.core.presentation.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,14 +11,15 @@ import androidx.viewbinding.ViewBinding
 class GenericAdapter<ItemT : Any, VB : ViewBinding>(
 	private val inflate: (LayoutInflater, ViewGroup, Boolean) -> VB,
 	list: List<ItemT> = emptyList(),
-	private val onBind: VB.(ItemT) -> Unit,
+	private val areItemsTheSame: (oldItem: ItemT, newItem: ItemT) -> Boolean = { oldItem, newItem -> oldItem == newItem },
+	private val areContentsTheSame: (oldItem: ItemT, newItem: ItemT) -> Boolean = { oldItem, newItem -> oldItem == newItem },
+	private val onBind: VB.(item: ItemT) -> Unit,
 ) : ListAdapter<ItemT, GenericAdapter<ItemT, VB>.ViewHolder>(
 	object : DiffUtil.ItemCallback<ItemT>()
 	{
-		override fun areItemsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
+		override fun areItemsTheSame(oldItem: ItemT, newItem: ItemT) = areItemsTheSame(oldItem, newItem)
 
-		@SuppressLint("DiffUtilEquals")
-		override fun areContentsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
+		override fun areContentsTheSame(oldItem: ItemT, newItem: ItemT) = areContentsTheSame(oldItem, newItem)
 	}
 )
 {
@@ -48,37 +48,3 @@ class GenericAdapter<ItemT : Any, VB : ViewBinding>(
 		onBind(holder.binding, item)
 	}
 }
-
-//class GenericAdapter<ItemT : Any, VB : ViewBinding>(
-//	@LayoutRes private val layoutRes: Int,
-//	private val bind: (View) -> VB,
-//	private val onBind: VB.(ItemT) -> Unit,
-//) : ListAdapter<ItemT, GenericAdapter<ItemT, VB>.ViewHolder>(
-//	object : DiffUtil.ItemCallback<ItemT>()
-//	{
-//		override fun areItemsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
-//
-//		@SuppressLint("DiffUtilEquals")
-//		override fun areContentsTheSame(oldItem: ItemT, newItem: ItemT) = oldItem == newItem
-//	}
-//)
-//{
-//	inner class ViewHolder(view: View, val binding: VB) : RecyclerView.ViewHolder(view)
-//
-//
-//	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-//	{
-//		val view = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-//
-//		val binding = bind(view)
-//
-//		return ViewHolder(binding.root, binding)
-//	}
-//
-//	override fun onBindViewHolder(holder: ViewHolder, position: Int)
-//	{
-//		val item = getItem(position)
-//
-//		onBind(holder.binding, item)
-//	}
-//}
