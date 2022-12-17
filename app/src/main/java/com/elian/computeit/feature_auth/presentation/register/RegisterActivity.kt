ef14ef2b9d412e06551877ff108cb6447157c111
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-12-17 19:02:07 +0100

    Update RegisterActivity.kt

--- app/src/main/java/com/elian/computeit/feature_auth/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/elian/computeit/feature_auth/presentation/register/RegisterActivity.kt
@@ -46,12 +46,12 @@ class RegisterActivity : AppCompatActivity()
 
 	private fun initUi() = using(binding)
 	{
+		// This is to set the desired behaviour when typing
+		tietUsername.setRawInputType(InputType.TYPE_CLASS_TEXT)
+		
 		tietUsername.addTextChangedListener { viewModel.onAction(EnterUsername("$it".trim())) }
 		tietPassword.addTextChangedListener { viewModel.onAction(EnterPassword("$it".trim())) }
 		tietConfirmPassword.addTextChangedListener { viewModel.onAction(EnterConfirmPassword("$it".trim())) }
-
-		// This is to set the desired behaviour when typing
-		tietUsername.setRawInputType(InputType.TYPE_CLASS_TEXT)
 
 		btnRegister.setOnClickListener { viewModel.onAction(Register) }
 	}
