a6710677efe7d7f404eebfbab60870917d7e11ca
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2023-01-15 19:42:20 +0100

    Delete OperationState.kt

--- app/src/main/java/com/elian/computeit/feature_tests/presentation/test/OperationState.kt
+++
@@ -1,8 +0,0 @@
-package com.elian.computeit.feature_tests.presentation.test
-
-import com.elian.computeit.core.domain.models.NumberPair
-
-data class OperationState(
-	val pairOfNumbers: NumberPair = NumberPair(),
-	val symbol: String = "",
-)
