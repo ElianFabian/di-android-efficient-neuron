1b86861289cad20186c7bbdad8ee35a09647a896
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2023-01-20 21:04:34 +0100

    Updated tests depdencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,6 @@ dependencies {
 	implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 	implementation("androidx.preference:preference-ktx:1.2.0")
 	testImplementation("junit:junit:")
-	androidTestImplementation("androidx.test.ext:junit:1.1.4")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+	androidTestImplementation("androidx.test.ext:junit:1.1.5")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
