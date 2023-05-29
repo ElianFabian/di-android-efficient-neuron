11ee15a151e46ef5ad950ec6b92291ce07c65a41
Elián Fabián <86477169+ElianFabian@users.noreply.github.com>
2023-05-29 21:55:55 +0200

    Update SimpleBottomSheetDialogFragment.kt

--- app/src/main/java/com/elian/computeit/core/presentation/util/SimpleBottomSheetDialogFragment.kt
+++ app/src/main/java/com/elian/computeit/core/presentation/util/SimpleBottomSheetDialogFragment.kt
@@ -273,8 +273,8 @@ abstract class SimpleBottomSheetDialogFragment<TArgs : Parcelable, TEvent : Parc
 		 * Returns a new instance of the given SimpleBottomSheetDialog in getNewInstance
 		 * with the specified id and args.
 		 *
-		 * @param id Should identifies a single `SimpleBottomSheetDialogFragment` instance, use it when you multiple instances
-		 * in the same fragment or activity to allow events work properly.
+		 * @param id Should identify a single `SimpleBottomSheetDialogFragment` instance. Use it when multiple instances
+		 * are required in the same fragment or activity to allow a correct behavior.
 		 * @param args The data to pass when instantiating the dialog.
 		 * @param getNewInstance A lambda function that creates the instance of the desired `SimpleBottomSheetDialogFragment`.
 		 *
