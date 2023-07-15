01f6fb371ec56012cc07b6a4230557e1e8271571
Elián Fabián <elian.fabian.el.belaizi@gmail.com>
2023-07-15 20:11:02 +0200

    [UPDATE]: upgraded depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ plugins {
 
 android {
 	namespace = "com.elian.computeit"
-	compileSdk = 33 // DON'T UPDATE (32)
+	compileSdk = 33
 
 	defaultConfig {
 		applicationId = "com.elian.computeit"
 		minSdk = 23
-		targetSdk = 33 // DON'T UPDATE (32)
+		targetSdk = 33
 		versionCode = 1
 		versionName = "0.5"
 
@@ -65,15 +65,15 @@ dependencies {
 	implementation("androidx.datastore:datastore-preferences:1.0.0")
 
 	// Dagger Hilt
-	implementation("com.google.dagger:hilt-android:2.43")
-	kapt("com.google.dagger:hilt-compiler:2.43")
+	implementation("com.google.dagger:hilt-android:2.44")
+	kapt("com.google.dagger:hilt-compiler:2.47")
 	kapt("androidx.hilt:hilt-compiler:1.0.0")
 
 	//Firebase Firestore
 	implementation("com.google.firebase:firebase-firestore-ktx")
 
 	// Firebase Storage
-	implementation("com.google.firebase:firebase-storage-ktx:20.1.0")
+	implementation("com.google.firebase:firebase-storage-ktx:20.1.0") //DON'T UPDATE (20.1.0) error when upgrading (java.lang.NoClassDefFoundError: Failed resolution of: Lcom/google/firebase/appcheck/interop/InternalAppCheckTokenProvider;)
 
 	// Import the Firebase BoM
 	implementation(platform("com.google.firebase:firebase-bom:29.3.1"))
@@ -96,14 +96,14 @@ dependencies {
 	implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
 
-	implementation("androidx.activity:activity-ktx:1.5.0") // DON'T UPDATE (1.5.0)
-	implementation("androidx.core:core-ktx:1.8.0") // DON'T UPDATE (1.8.0)
-	implementation("androidx.appcompat:appcompat:1.5.1") // DON'T UPDATE (1.5.1)
-	implementation("com.google.android.material:material:1.7.0")
+	implementation("com.google.android.material:material:1.9.0")
+	implementation("androidx.activity:activity-ktx:1.5.0") // DON'T UPDATE (1.5.0)  error when upgrading (conflict)
+	implementation("androidx.core:core-ktx:1.8.0") // DON'T UPDATE (1.8.0) error when upgrading (conflict)
+	implementation("androidx.appcompat:appcompat:1.6.1") // DON'T UPDATE (1.5.1)
 	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 	implementation("androidx.legacy:legacy-support-v4:1.0.0")
-	implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-	implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+	implementation("androidx.navigation:navigation-fragment-ktx:2.5.3") // error when upgrading (conflict)
+	implementation("androidx.navigation:navigation-ui-ktx:2.5.3") // error when upgrading (conflict)
 	implementation("androidx.preference:preference-ktx:1.2.0")
 	testImplementation("junit:junit:")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+		classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
 
 		// Firebase
 		classpath("com.google.gms:google-services:4.3.15")
-		classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
+		classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.7")
 
 		classpath("com.android.tools.build:gradle:8.0.2")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
